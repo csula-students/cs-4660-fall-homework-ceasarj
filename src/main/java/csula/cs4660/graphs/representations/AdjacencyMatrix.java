285ddf88862ea5052f7995ccc17e13ba081bd3ba
ceejay <cvvjimenez@gmail.com>
2016-09-28 23:53:02 -0700

    Fixed merge errors

--- src/main/java/csula/cs4660/graphs/representations/AdjacencyMatrix.java
+++ src/main/java/csula/cs4660/graphs/representations/AdjacencyMatrix.java
@@ -21,19 +21,13 @@ public class AdjacencyMatrix implements Representation {
     private int[][] adjacencyMatrix;
 
     public AdjacencyMatrix(File file) {
-<<<<<<< HEAD
-=======
 
->>>>>>> master
         List<List<Integer>> matrixList = ParserUtil.readFile(file);
         int numOfNodes = matrixList.get(0).get(0);
         nodes = new Node[numOfNodes];
         adjacencyMatrix = new int[numOfNodes][numOfNodes];
         convertList(matrixList);
-<<<<<<< HEAD
-=======
 
->>>>>>> master
     }
 
     public AdjacencyMatrix() {
@@ -45,27 +39,17 @@ public class AdjacencyMatrix implements Representation {
 
     @Override
     public boolean adjacent(Node x, Node y) {
-<<<<<<< HEAD
         return adjacencyMatrix[(Integer)x.getData()][(Integer)y.getData()] > 0;
-=======
-        Integer xValue = (Integer)x.getData();
-        Integer yValue = (Integer)y.getData();
-        return adjacencyMatrix[xValue][yValue] > 0;
->>>>>>> master
+
     }
 
     @Override
     public List<Node> neighbors(Node x) {
-<<<<<<< HEAD
+
         Integer xValue = (Integer)x.getData();
         List<Node> neighbors = Lists.newArrayList();
         for(int yValue=0; yValue<adjacencyMatrix[0].length; yValue++) {
-=======
-        System.out.println(toString() + "\n" + x.getData());
-        Integer xValue = (Integer)x.getData();
-        List<Node> neighbors = Lists.newArrayList();
-        for(int yValue=0; yValue<adjacencyMatrix[xValue].length; yValue++) {
->>>>>>> master
+
             if(adjacencyMatrix[xValue][yValue] > 0) {
                 neighbors.add(nodes[yValue]);
             }
@@ -76,7 +60,6 @@ public class AdjacencyMatrix implements Representation {
     @Override
     public boolean addNode(Node x) {
         Integer value = (Integer)x.getData();
-<<<<<<< HEAD
         if(value >= nodes.length) { //out of bounds
             resizeNodes(x);
             resizeMatrix(x);
@@ -88,21 +71,6 @@ public class AdjacencyMatrix implements Representation {
             return false;
         }
         return true;
-=======
-        if(value >= nodes.length){ //out of bounds
-            resizeNodes(x);
-            resizeMatrix(x);
-
-            return true;
-        }
-        if(nodes[value] == null ){
-            nodes[value] = x;
-
-            return true;
-        }
-
-        return false;
->>>>>>> master
     }
 
     @Override
@@ -134,10 +102,6 @@ public class AdjacencyMatrix implements Representation {
         }
         if(adjacencyMatrix[fromValue][toValue] != x.getValue()){ // edge is not already there
             adjacencyMatrix[fromValue][toValue] = x.getValue();
-<<<<<<< HEAD
-=======
-            adjacencyMatrix[toValue][fromValue] = x.getValue();
->>>>>>> master
             return true;
         }
 
@@ -156,26 +120,16 @@ public class AdjacencyMatrix implements Representation {
         }
         if(adjacencyMatrix[fromValue][toValue] > 0){ // edge is already there
             adjacencyMatrix[fromValue][toValue] = 0;
-<<<<<<< HEAD
-            return true;
-        }
-=======
-            adjacencyMatrix[toValue][fromValue] = 0;
+
             return true;
         }
 
->>>>>>> master
         return false;
     }
 
     public int distance(Node from, Node to) {
-<<<<<<< HEAD
         return adjacencyMatrix[(Integer)from.getData()][(Integer)to.getData()];
-=======
-        Integer fromValue = (Integer)from.getData();
-        Integer toValue = (Integer)to.getData();
-        return adjacencyMatrix[fromValue][toValue];
->>>>>>> master
+
     }
 
     @Override
@@ -186,10 +140,7 @@ public class AdjacencyMatrix implements Representation {
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
-<<<<<<< HEAD
-=======
 
->>>>>>> master
         for(int i = 0; i<adjacencyMatrix.length; i++){
             for(int j = 0; j<adjacencyMatrix.length; j++){
                 sb.append(adjacencyMatrix[i][j]);
@@ -197,10 +148,6 @@ public class AdjacencyMatrix implements Representation {
             }
             sb.append("\n");
         }
-<<<<<<< HEAD
-=======
-
->>>>>>> master
         return sb.toString();
     }
 
@@ -216,7 +163,6 @@ public class AdjacencyMatrix implements Representation {
         int[][] newAdjacencyMatrix = new int[value + 1][value + 1];
 
         // copy adacency matrix to new adjacency matrix
-<<<<<<< HEAD
         for(int row=0; row<adjacencyMatrix.length; row++)
             for(int col=0; col<adjacencyMatrix[row].length; col++)
                 newAdjacencyMatrix[row][col] = adjacencyMatrix[row][col];
@@ -244,44 +190,6 @@ public class AdjacencyMatrix implements Representation {
 
                 if(nodes[nodeFromData] == null)
                     nodes[nodeFromData] = new Node(nodeFromData);
-            }
-
-        });
-=======
-        for(int row=0; row<adjacencyMatrix.length; row++){
-            for(int col=0; col<adjacencyMatrix[row].length; col++){
-                newAdjacencyMatrix[row][col] = adjacencyMatrix[row][col];
-            }
-        }
-
-        adjacencyMatrix = newAdjacencyMatrix;
->>>>>>> master
-    }
-
-    private void resizeNodes(Node node){
-        Integer value = (Integer) node.getData();
-        Node[] newNodes = new Node[value + 1];
-        newNodes[value] = node;
-        for(int i=0; i<nodes.length; i++){
-            newNodes[i] = nodes[i];
-        }
-    }
-
-    private void convertList(List<List<Integer>> numbers){
-        numbers.forEach(line ->{
-            // skip the first line because it is the size of matrix nxn
-            if(line.size() > 1){
-                int nodeFromData = line.get(0);
-                int nodeToData = line.get(1);
-
-                adjacencyMatrix[nodeFromData][nodeToData] = line.get(2);
-                adjacencyMatrix[nodeToData][nodeFromData] = line.get(2);
-
-                if(nodes[nodeFromData] == null)
-                    nodes[nodeFromData] = new Node(nodeFromData);
-
-                if(nodes[nodeToData] == null)
-                    nodes[nodeToData] = new Node(nodeToData);
             }
 
         });
