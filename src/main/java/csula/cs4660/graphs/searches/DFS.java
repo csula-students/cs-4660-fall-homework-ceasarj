fad7e186a95918f1b684b3507f9e7e24630cc229
ceejay <cvvjimenez@gmail.com>
2016-10-12 22:49:31 -0700

    Add DFS

--- src/main/java/csula/cs4660/graphs/searches/DFS.java
+++ src/main/java/csula/cs4660/graphs/searches/DFS.java
@@ -1,10 +1,13 @@
 package csula.cs4660.graphs.searches;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Depth first search
@@ -12,6 +15,43 @@ import java.util.List;
 public class DFS implements SearchStrategy {
     @Override
     public List<Edge> search(Graph graph, Node source, Node dist) {
-        return null;
+        Map<Node, Edge> parent = Maps.newHashMap();
+        Stack<Node> s = new Stack<>();
+        Set<Node> v = Sets.newHashSet();
+        List<Edge> path = Lists.newArrayList();
+
+        s.push(source);
+        v.add(source);
+
+        Node current = source;
+
+        while(!current.equals(dist)){
+            List<Node> n = graph.neighbors(current);
+            boolean noNeighbors = true;
+            for(Node neighbor: n){
+                if(!v.contains(neighbor)){
+                    s.add(neighbor);
+                    v.add(neighbor);
+                    parent.put(
+                            neighbor,
+                            new Edge(current, neighbor, graph.distance(current, neighbor))
+                    );
+                    current = neighbor;
+                    noNeighbors = false;
+                    break;
+                }
+            }
+            if(noNeighbors) current = s.pop();
+        }
+
+        // backtrack
+        Node curr = dist;
+        do{
+            Edge e = parent.get(curr);
+            path.add(e);
+            curr = e.getFrom();
+        }while(!curr.equals(source));
+        Collections.reverse(path);
+        return path;
     }
 }
